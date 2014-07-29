b9a27eb2005c98d7301461afa2b8122ab1ab87e3
Arul15 <ntlbusinessapp@sixcroresapp.local>
2014-07-29 14:53:42 +0530

    Initial Commit

---
+++ JsonParser/JsonAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JsonAppDelegate.h
+//  JsonParser
+//
+//  Created by NTL BUSINESS APP on 7/29/14.
+//  Copyright (c) 2014 NTL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JsonAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JsonParser/JsonViewController.h
@@ -0,0 +1,13 @@
+//
+//  JsonViewController.h
+//  JsonParser
+//
+//  Created by NTL BUSINESS APP on 7/29/14.
+//  Copyright (c) 2014 NTL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JsonViewController : UIViewController
+
+@end
